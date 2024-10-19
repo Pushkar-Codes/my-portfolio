e5af5d684e4e08f81fb6145b44598f40dfe428a1
Pushkar Dutta <pushkardutt555@gmail.com>
2024-10-19 21:52:38 +0530

    fevicon updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import Head from "next/head"; // Correct import
 import Homepage from "./home";
 
 // Main component
 export default function Home() {
   return (
     <main>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Homepage />
     </main>
   );
